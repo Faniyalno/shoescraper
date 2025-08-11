7203203f241849bd2cc3277ad28953b4373128ab
Fani <faniyalnoarkanrahman@gmail.com>
2025-08-11 20:04:41 +0700

    creating a spider that scrape catalogue-list

---
+++ shoescraper/spiders/spiderfootwear.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class SpiderfootwearSpider(scrapy.Spider):
+    name = "spiderfootwear"
+    allowed_domains = ["pyoppfledge.com"]
+    start_urls = ["https://pyoppfledge.com/collections/barefoot-shoes"]
+
+    cusom_settings = {
+        'DOWNLOAD_DELAY' : 2,
+    }
+
+    def parse(self, response):
+        footwears = response.css('div.product-item')
+        for footwear in footwears:
+            yield{
+                'Name' : footwear.css('h4 a::text').get(),
+                'price' : footwear.css('span.money::text').get(),
+                'url' : footwear.css('h4 a::attr(href)').get(),
+            }
