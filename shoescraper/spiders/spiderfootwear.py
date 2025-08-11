5a04eb3f73c657927c702ebf95e84718d4eada01
Fani <faniyalnoarkanrahman@gmail.com>
2025-08-11 22:58:22 +0700

    capture the next page

--- shoescraper/spiders/spiderfootwear.py
+++ shoescraper/spiders/spiderfootwear.py
@@ -14,3 +14,8 @@ class SpiderfootwearSpider(scrapy.Spider):
                 'price' : footwear.css('span.money::text').get(),
                 'url' : footwear.css('h4 a::attr(href)').get(),
             }
+
+        next_page = response.css('li.pagination__item a.link::attr(href)').get().split('&')[0]
+        if next_page is not None:
+            next_page_url = 'https://pyoppfledge.com' + next_page
+        yield response.follow(next_page_url, callback=self.parse)
