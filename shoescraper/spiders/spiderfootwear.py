37e183ef8b2dca3063b6fa4567ae5beeec560f77
Fani <faniyalnoarkanrahman@gmail.com>
2025-08-12 00:32:05 +0700

    Fixed next page logic

--- shoescraper/spiders/spiderfootwear.py
+++ shoescraper/spiders/spiderfootwear.py
@@ -4,7 +4,7 @@ import scrapy
 class SpiderfootwearSpider(scrapy.Spider):
     name = "spiderfootwear"
     allowed_domains = ["pyoppfledge.com"]
-    start_urls = ["https://pyoppfledge.com/collections/barefoot-shoes"]
+    start_urls = ["https://pyoppfledge.com/collections/barefoot-shoes?page=1"]
 
     def parse(self, response):
         footwears = response.css('div.product-item')
@@ -15,7 +15,12 @@ class SpiderfootwearSpider(scrapy.Spider):
                 'url' : footwear.css('h4 a::attr(href)').get(),
             }
 
-        next_page = response.css('li.pagination__item a.link::attr(href)').get().split('&')[0]
-        if next_page is not None:
-            next_page_url = 'https://pyoppfledge.com' + next_page
-        yield response.follow(next_page_url, callback=self.parse)
+        next_element = response.css('a.pagination__touch::attr(title)').get().split(' ')[0].lower()
+        print(next_element)
+
+        if next_element == 'next':
+            next_page = response.css('li.pagination__item a.link::attr(href)').get().split('&')[0]
+            if next_page is not None:
+                next_page_url = 'https://pyoppfledge.com' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
+            
