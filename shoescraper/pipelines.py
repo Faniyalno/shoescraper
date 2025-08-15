50048f6febd66f3e7a72a5563002483712f4cb8b
Fani <faniyalnoarkanrahman@gmail.com>
2025-08-16 03:22:36 +0700

    normalised size list in pipelines

--- shoescraper/pipelines.py
+++ shoescraper/pipelines.py
@@ -10,4 +10,15 @@ from itemadapter import ItemAdapter
 
 class ShoescraperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+    
+        #normalized data
+        #Strip whitespace & set it to float on size lists
+        size_lists = adapter.get('size_lists')
+        print("*******************************")
+        print(size_lists)
+        trimmed = [trim.strip() for trim in size_lists[0]] 
+        float_trimmed = [float(f) for f in trimmed]
+        print(float_trimmed)
+        adapter['size_lists'] = float_trimmed
         return item
